45957436293604e2409317708f73181c83975287
liudong.li <liudong.li@foxmail.com>
2023-07-20 22:36:55 +0800

    网关

---
+++ gateway/src/main/java/com/itheima/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.itheima.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GatewayApplication
+ *
+ * @author liliudong
+ * @version 1.0
+ * @description
+ * @date 2023/7/20 20:19
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
